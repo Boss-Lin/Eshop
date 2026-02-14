30d5303ae24cef5823e88acedaeed3278daf4c94
Ben <bbosstw@gmail.com>
2026-02-14 17:54:19 +0800

    add example value

--- Models/Request/LoginRequest.cs
+++ Models/Request/LoginRequest.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace EShop.Models.Request;
@@ -6,7 +7,9 @@ public class LoginRequest
 {
     [Required]
     [DataType(DataType.EmailAddress)]
+    [DefaultValue("customer1@example.com")]
     public string Email { get; set; } = string.Empty;[Required]
     [DataType(DataType.Password)]
+    [DefaultValue("Customer123!")]
     public string Password { get; set; } = string.Empty;
 }
